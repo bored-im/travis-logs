3dc278bf9e2a94c016624d7d7f927aea43e57dce
Yuva <yuva@codemancers.com>
2018-09-14 08:45:52 +0530

    remove metriks code

--- lib/travis/logs/metrics_methods.rb
+++ lib/travis/logs/metrics_methods.rb
@@ -6,15 +6,9 @@ module Travis
   module Logs
     module MetricsMethods
       def measure(name = nil, &block)
-        timer_name = [self.class.metriks_prefix, name].compact.join('.')
-        Metriks.timer(timer_name).time(&block)
-      rescue StandardError
-        failed_name = [name, 'failed'].compact.join('.')
-        mark(failed_name)
       end
 
       def mark(name)
-        Metriks.meter("#{self.class.metriks_prefix}.#{name}").mark
       end
     end
   end
